e76d75ac3ce75036afd2a920e69de65b23a21d54
adriannnvd <adriandevilla0518@gmail.com>
2024-03-20 17:47:51 +0800

    login system second commit

--- login_system_views/views/views_main.go
+++ login_system_views/views/views_main.go
@@ -1,14 +1,27 @@
 package views
 
 import (
-	"net/http"
-	"strings"
+    "net/http"
+    "strings"
+
+	"github.com/uadmin/uadmin"
 )
 
+// MainHandler is the main handler for the login system.
 func MainHandler(w http.ResponseWriter, r *http.Request) {
+    // r.URL.Path creates a new path called "/login_system/"
+    r.URL.Path = strings.TrimPrefix(r.URL.Path, "/login_system")
+    r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
 
-	r.URL.Path = strings.TrimPrefix(r.URL.Path, "/logim_system")
-	r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+    // Authentication : This session is preloaded with a user.
+    session := uadmin.IsAuthenticated(r)
+    if session == nil {
+        // LoginHandler verifies login data and creating sessions for users.
+        LoginHandler(w, r)
+        return
+    }
 
-	LoginHandler(w, r)
+    // HomeHandler handles the home page.
+    HomeHandler(w, r, session)
+    return
 }
